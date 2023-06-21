18a29ea0a01242b5046386af658a021dff3f0ab4
Nam Ha <tokarinrika@gmail.com>
2023-06-21 16:04:55 +0700

    Add new models

---
+++ SVM/config.py
@@ -0,0 +1,13 @@
+# General configuration
+TRAIN_FILE = "../data/train.csv"
+
+TEST_FILE = "../data/test.csv"
+
+SUBMISSION_FILE = "result/submission.csv"
+
+EVAL_FILE = "../eval.xlsx"
+
+TRAIN_PROPORTION = 0.8
+
+# MODEL CONFIGURATION
+NUM_NEIGHBORS = 1
