ef8187950f25ea0b6cb4c5d23ce9dda6f51fa202
Nam Ha <tokarinrika@gmail.com>
2023-06-25 16:52:05 +0700

    Update

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -6,6 +6,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+import time
+
 from config import *
 
 """
@@ -53,8 +55,11 @@ print()
 """
     TRAIN MODEL!
 """
-model.fit(x, y)
+start_training_time = time.time()
+model.fit(x_train, y_train)
+end_training_time = time.time()
 
+print("Training time: {}s".format(end_training_time - start_training_time))
 
 """
     EVALUATE ON TRAIN AND VAL DATASET
@@ -73,7 +78,11 @@ print()
 """
     TEST MODEL
 """
+start_inferring_time = time.time()
 y_predicted_test = model.predict(x_test).reshape(-1, 1)
+end_inferring_time = time.time()
+print("Inference time: {}s".format(end_inferring_time - start_inferring_time))
+
 y_predicted_test_no = np.arange(1, x_test.shape[0] + 1).reshape(-1, 1)
 
 solution = np.concatenate([y_predicted_test_no, y_predicted_test], axis = 1)
--- RandomForest/RandomForest.py
+++ RandomForest/RandomForest.py
@@ -6,6 +6,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+import time
+
 from config import *
 
 """
@@ -40,6 +42,8 @@ print()
 """
 model = RandomForestClassifier(
     n_estimators = NUM_ESTIMATORS,
+    min_samples_split = 9,
+    min_samples_leaf = 3,
     random_state = 0
 )
 
@@ -54,9 +58,11 @@ print()
 """
     TRAIN MODEL!
 """
-model.fit(x, y)
+start_training_time = time.time()
+model.fit(x_train, y_train)
+end_training_time = time.time()
 
-
+print("Training time: {}s".format(end_training_time - start_training_time))
 """
     EVALUATE ON TRAIN AND VAL DATASET
 """
@@ -74,7 +80,11 @@ print()
 """
     TEST MODEL
 """
+start_inferring_time = time.time()
 y_predicted_test = model.predict(x_test).reshape(-1, 1)
+end_inferring_time = time.time()
+print("Inference time: {}s".format(end_inferring_time - start_inferring_time))
+
 y_predicted_test_no = np.arange(1, x_test.shape[0] + 1).reshape(-1, 1)
 
 solution = np.concatenate([y_predicted_test_no, y_predicted_test], axis = 1)
